cbc49c16e087ff39980ee22bfbbceb648cbe8060
adityacodes30 <adityaework@gmail.com>
2023-08-23 01:05:29 +0530

    initial files

---
+++ README.md
@@ -0,0 +1,1 @@
+# NODE api with postgres modelled on a website
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import app from './server'
+
+app.listen(3001 , ()=>
+{
+    console.log('on port 3001');
+})
+
+
+// also learning orm (object relational mapping)(oop for databases basically)- PRISMA in this course , database agnostic
