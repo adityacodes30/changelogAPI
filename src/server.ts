d8d28a5821fe7508648bb92330aec5c3f7b32b61
adityacodes30 <adityaework@gmail.com>
2023-08-23 16:41:24 +0530

    auth setup

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import express from 'express'
 import router from './router';
 import morgan from 'morgan'
+import cors from 'cors'
 
 const app = express();
 
+//middlware
+app.use(cors)
 app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
 
+//routes
 app.get("/", (req, res) => {
     console.log('hello express');
     res.status(200);
